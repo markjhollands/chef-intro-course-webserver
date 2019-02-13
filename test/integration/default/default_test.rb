c3c5e6ac69f39d4734fc04e1f35326c98bcda940
Mark Hollands <markjhollands@gmail.com>
2019-02-13 23:22:26 +0000

    Add some basic tests

--- test/integration/default/default_test.rb
+++ test/integration/default/default_test.rb
@@ -5,14 +5,10 @@
 # The Inspec reference, with examples and extensive documentation, can be
 # found at http://inspec.io/docs/reference/resources/
 
-unless os.windows?
-  # This is an example test, replace with your own test.
-  describe user('root'), :skip do
-    it { should exist }
-  end
-end
+describe port(80) do
+  it { should be_listening }
+done
 
-# This is an example test, replace it with your own test.
-describe port(80), :skip do
-  it { should_not be_listening }
+describe command('curl localhost') do
+  its('stdout') { should match('Hello, world') }
 end
