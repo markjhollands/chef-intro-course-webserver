b3e19feba5cc7cfe18b4b56cfbcb6ed5ca2a0cd8
Mark Hollands <markjhollands@gmail.com>
2019-02-14 00:34:43 +0000

    Update file in cookbook with ohai attributes

--- recipes/default.rb
+++ recipes/default.rb
@@ -9,7 +9,13 @@ package 'httpd' do
 end
 
 file '/var/www/html/index.html' do
-  content '<h1>Hello, world!</h1>'
+  content "<h1>Hello, world!</h1>
+  
+  <h2>IPADDRESS: #{node['ipaddress']}</h2>
+  <h2>HOSTNAME: #{node['hostname']}</h2>
+  <h2>MEMORY: #{node['memory']['total']}</h2>
+  <h2>CPU: #{node['cpu']['0']['mhz']}</h2>
+"
 end
 
 service 'httpd' do
